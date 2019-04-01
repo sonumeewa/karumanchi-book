32921fef41fd8127fe6bc1bdb40f2ff611a79abe
sonumeewa <sonumeewa@gmail.com>
2019-04-01 14:07:35 +0530

    circularlinkedlist

--- Chapter-3/circularlinkedlist.cpp
+++ Chapter-3/circularlinkedlist.cpp
@@ -16,6 +16,83 @@ int listLength(listNode * head)
 	}while(temp!=head);
 	return count;
 }
+void insertEnd(listNode ** head, int data)
+{
+	listNode * current = *head;
+	listNode* newNode = (listNode *) malloc(sizeof(listNode));
+	if(!newNode)
+	{
+		cout<<"memory error"<<endl;
+		return;
+	}
+	newNode->data =data;
+	while(current->next !=*head)
+	current = current->next;
+	newNode->next = *head;
+	current->next = newNode;
+}
+void insertBeg(listNode ** head, int data)
+{
+	listNode* newNode = (listNode *) malloc(sizeof(listNode));
+	if(!newNode)
+	{
+		cout<<"memory error"<<endl;
+		return;
+	}
+	
+	if(!head)
+	{
+		*head=newNode;
+		newNode->data = data;
+		newNode->next=NULL;
+	}
+	else
+	{
+		listNode * cur;
+		newNode->data = data;
+		newNode->next= *head;
+		while(cur->next != *head)
+		cur = cur->next;
+		cur->next = newNode;
+		*head = newNode;
+	}
+}
+void deleteEnd(listNode ** head)
+{
+	listNode * temp, * cur;
+	if(!head)
+	{
+		cout<<"Empty List"<<endl;
+		return;
+	}
+	cur = *head;
+	while(cur->next != *head)
+	{
+		temp = cur;
+		cur=cur->next;
+	}
+	temp->next = cur->next;
+	free(cur);
+}
+void deleteBeg(listNode ** head)
+{
+	listNode * temp, * cur;
+	if(!head)
+	{
+		cout<<"Empty List"<<endl;
+		return;
+	}
+	temp = *head;
+	cur = *head;
+	while(cur->next != *head)
+	{
+		temp = cur;
+		cur=cur->next;
+	}
+	cur->next = temp->next;
+	*head = (*head)->next;
+	free(temp);
+}
 int main()
 {
 	
